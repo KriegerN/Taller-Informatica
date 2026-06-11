a991d1f4e4038ad01a7eafa642b50208ce4610ac
Nicolas Guerrero <kriegern@users.noreply.github.com>
2026-06-11 16:50:57 -0400

    Actualización de index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Taller Informatica</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+            <h1>Integrantes:</h1>
+            <ul>
+                <li> Alier Fres Zúñiga</li>
+                <li> Nicolas Andrés Guerrero Ulloa</li>
+                <li> Francisco Joaquin Nuñez Varas</li>
+                <li> Matias Javier Agüero Railaf</li>
+            </ul>
+            <h1>Tema: Gestion de Usuarios</h1>
+            <p>Descripcion de la aplicacion: Se trata de un sistema de gestion de usuarios donde puede gestionar los usuarios de una universidad </p>
+            <p>Descripcion de las operaciones CRUD:  Se puede visualizar la información de los usuarios (READ), modificarla (UPDATE), eliminarla (DELETE) y añadir nuevos usuarios (CREATE)</p>
+    </body> 
+</html>
