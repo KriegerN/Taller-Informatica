31092fde2addc11140e9bde61e6c5afb0f44dd89
Nicolas Guerrero <kriegern@users.noreply.github.com>
2026-06-11 16:57:55 -0400

    Corrección de errores ortográficos

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Taller Informatica</title>
+        <title>Taller Informática</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="style.css">
     </head>
@@ -11,12 +11,12 @@
             <h1>Integrantes:</h1>
             <ul>
                 <li> Alier Fres Zúñiga</li>
-                <li> Nicolas Andrés Guerrero Ulloa</li>
-                <li> Francisco Joaquin Nuñez Varas</li>
+                <li> Nicolás Andrés Guerrero Ulloa</li>
+                <li> Francisco Joaquin Núñez Varas</li>
                 <li> Matias Javier Agüero Railaf</li>
             </ul>
-            <h1>Tema: Gestion de Usuarios</h1>
-            <p>Descripcion de la aplicacion: Se trata de un sistema de gestion de usuarios donde puede gestionar los usuarios de una universidad </p>
+            <h1>Tema: Gestión de Usuarios - Universidad</h1>
+            <p>Descripcion de la aplicación: Se trata de un sistema de gestion de usuarios donde puede gestionar los usuarios de una universidad </p>
             <p>Descripcion de las operaciones CRUD:  Se puede visualizar la información de los usuarios (READ), modificarla (UPDATE), eliminarla (DELETE) y añadir nuevos usuarios (CREATE)</p>
     </body> 
 </html>
