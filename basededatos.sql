ce8916ab82e667d2f08daaf3c62d6abc2f23018b
Nico <128738154+KriegerN@users.noreply.github.com>
2026-07-08 22:38:02 +0000

    mejores comentarios

--- basededatos.sql
+++ basededatos.sql
@@ -1,15 +1,20 @@
--- tabla universidad_db
--- usuario postgres
+-- base de datos: universidad_db
+-- usuario: postgres
+-- contraseña: admin
+-- comando para la base de datos en consola: psql -h localhost -U postgres -d universidad_db
+
 -- Tabla rol
 CREATE TABLE rol (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL UNIQUE
 );
+
 -- Tabla departamento
 CREATE TABLE departamento (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL UNIQUE
 );
+
 -- Tabla usuario
 CREATE TABLE usuario (
     rut VARCHAR(10) PRIMARY KEY,
@@ -27,6 +32,7 @@ CREATE TABLE usuario (
     ON DELETE CASCADE
 );
 
+-- Tabla registro (Bitácora)
 CREATE TABLE registro (
     id SERIAL PRIMARY KEY,
     tipo VARCHAR(50) NOT NULL,
@@ -47,3 +53,8 @@ INSERT INTO departamento (nombre) VALUES
 ('Informática'), 
 ('Derecho'), 
 ('Obstetricia');
+
+-- Usuario Administrador por defecto
+-- (La contraseña encriptada equivale a la palabra: admin)
+INSERT INTO usuario (rut, nombre, correo, password_hash, id_rol, id_departamento) VALUES 
+('1111111-1', 'Administrador Principal', 'admin@ulagos.cl', '$2y$10$/4JD6SW4o/gMC0mNmih4Het.y9fTWAhJpwRrUB7d1mdOoDUvFqXeq', 3, 1);
